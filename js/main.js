ce2994593e99bcc80be69c7dfa1ade368e455018
nukatha1996 <92627447+nukatha1996@users.noreply.github.com>
2023-03-14 16:57:49 +0700

    second version

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log('hello')
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const items = $$('.tab-item');
+const panes = $$('.tab-pane');
+const line = $('.tabs .line');
+
+line.style.left = $('.tab-item.active').offsetLeft + 'px';
+line.style.width = $('.tab-item.active').offsetWidth + 'px'; 
+
+
+items.forEach((item, index) => {
+    item.onclick = function() {
+        pane = panes[index];
+        $('.tab-item.active').classList.remove('active');
+        $('.tab-pane.active').classList.remove('active');
+        this.classList.add('active');
+        pane.classList.add('active');
+
+        line.style.left = this.offsetLeft + 'px';
+        line.style.width = this.offsetWidth + 'px'; 
+        
+
+    }
+})
